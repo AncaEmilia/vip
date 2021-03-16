d0f9a7e39dacd2c14a2754061ca00160749ebbed
AncaEmilia <AncaEmilia@users.noreply.github.com>
2021-03-16 20:51:38 +0200

    added responsive elements

--- contact.js
+++ contact.js
@@ -1,15 +1,20 @@
 mainPage.innerHTML += `
     
     <hr id="break">
-    <ul class="row d-flex justify-content-center p-0" id="contact"> 
-        <div class="row col-3 m-4"> 
-            <i class="fa fa-github text-center m-2" style="font-size:1.8vw;"> AncaEmilia</i>
-            <i class="fa fa-linkedin text-center m-2" style="font-size:1.8vw;"> Anca Trifanov</i>
+    <h1 class="text-center m-0" id="chapter4" style="font-size:3.5vw;">Contact</h1> 
+    <div class="row d-flex justify-content-evenly mt-3 contact">
+        <div class="col-xs-12 col-md-3 m-1 contact-item"> 
+            <a class="fa-icon text-center m-2" href="https://github.com/" title="">
+            <i class="fa fa-github m-1" style="font-size:1.8vw;"> AncaEmilia</i>
+            </a>
+            <a class="fa-icon text-center m-2" href="https://www.linkedin.com/in/anca-trifanov-5997b6172/" title="">
+            <i class="fa fa-linkedin m-1" style="font-size:1.8vw;"> Anca Trifanov</i>
+            </a>
         </div> 
-        <h1 class="col-3 d-flex text-center justify-content-center " id="chapter4" style="font-size:3.5vw;">Contact</h1> 
-        <div class="row col-3  m-4">
-            <i class="fa fa-mobile-phone text-center m-2" style="font-size:1.8vw;"> 0770793369</i>
-            <i class="fa fa-envelope m-2 text-center aria-hidden="true"" style="font-size:1.8vw;"> ancatrifanov@gmail.com</i>
+        <div class="col-xs-12 col-md-3 m-1 contact-item">
+            <a class="fa-icon text-center m-2"><i class="fa fa-mobile-phone m-1" style="font-size:1.8vw;"> 0770793369</i></a>
+            <a class="fa-icon text-center m-2"><i class="fa fa-envelope aria-hidden="true m-1"" style="font-size:1.8vw;"> ancatrifanov@gmail.com</i></a>
         </div>
-    </ul>
+    </div>
+    
 `;
--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 mainPage.innerHTML += ` 
-    <div class="d-grid gap-2 col-4 offset-4 mt-3 mb-3" >
+    <div class="col-xs-12 col-md-4 offset-md-4 d-grid gap-2 mt-3 mb-3" >
         <button class="rounded-pill"  id="btn-footer"  onclick="document.location='#header'" style="font-size:1.5vw;">Acum ia-o de la capăt !</button>
     </div>
 `;
